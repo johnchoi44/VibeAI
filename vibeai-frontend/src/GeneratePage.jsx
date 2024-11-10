8cfa4a42f58f9230e2fa35c6386106522cacf5c4
DavisJiwooIm <dji5081@psu.edu>
2024-11-10 13:16:20 -0500

    Add all project files

--- vibeai-frontend/src/GeneratePage.jsx
+++ vibeai-frontend/src/GeneratePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './generate.css';
-import textPic from './assets/text.png'
-import musicPic from './assets/music.png'
+import textPic from './assets/text.png';
+import musicPic from './assets/music.png';
 
 function GeneratePage() {
+  const handleCreateSongClick = () => {
+    // localhost:3000으로 이동
+    window.location.href = 'http://localhost:3000';
+  };
+
   return (
     <div className="generate-page">
       <div className="generate-content">
@@ -13,26 +18,25 @@ function GeneratePage() {
       <div className="options-container">
         <Link to="/speechtotext">
           <div className="option">
-              <div className="image-wrapper">
-                <img src={textPic} alt="Read for You" className="option-image"></img>
-              </div>
-
+            <div className="image-wrapper">
+              <img src={textPic} alt="Read for You" className="option-image" />
+            </div>
             <h3>Read for you</h3>
             <p>Upload a document or type your text, select a voice model, and generate a customized audio narration.</p>
           </div>
         </Link>
-        <Link to="/create-your-song">
-          <div className="option">
-              <div className="image-wrapper">
-                <img src={musicPic} alt="Create Your Song" className="option-image"></img>
-              </div>
-            <h3>Create your song</h3>
-            <p>Upload a song, choose a model, and create a personalized version of the track.</p>
+        
+        <div className="option" onClick={handleCreateSongClick}>
+          <div className="image-wrapper">
+            <img src={musicPic} alt="Create Your Song" className="option-image" />
           </div>
-        </Link>
+          <h3>Create your song</h3>
+          <p>Upload a song, choose a model, and create a personalized version of the track.</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default GeneratePage;
+
