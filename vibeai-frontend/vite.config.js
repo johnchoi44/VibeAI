5db40f306c590a1b83f462add2a4b98bd9818184
John Choi <John@Johns-MackBook-Pro.local>
2024-11-11 17:50:31 -0500

    update

--- vibeai-frontend/vite.config.js
+++ vibeai-frontend/vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({ base: "/VibeAI", plugins: [react()], }) 
+export default defineConfig({
+  plugins: [react()],
+})
+
